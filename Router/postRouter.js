9f06f0806d421a47b9a6ec89171c7c4a6741c535
ayush <ayushgairola2002@gmail.com>
2024-11-18 19:23:53 +0530

    fixed router head error

--- Router/postRouter.js
+++ Router/postRouter.js
@@ -5,11 +5,8 @@ const postController = require("../controller/postController");
 
 postRouter.get("/posts", postController.data.getAllPosts)
 //  THE USER NEEDS TO VERIFY HIMSELF WITH THE TOKEN IN ORDER TO BE ABLE TO CREATE A POST 
-<<<<<<< HEAD
+
     .post("/newpost", auth.data.authMiddleware, postController.data.upload.single('images'), postController.data.createPost)
-=======
-    .post("/newpost", postController.data.upload.single('images'), postController.data.createPost)
->>>>>>> b7efff1 (fresh code)
     .delete("/post/:id", postController.data.DeletePost)
     .patch("/update/:id", postController.data.UpdatePost);
 
