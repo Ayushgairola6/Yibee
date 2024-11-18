58055d390d7ad0ba11b390b43097061071672948
ayush <ayushgairola2002@gmail.com>
2024-11-18 19:08:55 +0530

    fixed HEAD error

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,13 +1,4 @@
 const jwt = require('jsonwebtoken');
-<<<<<<< HEAD
-
-function authMiddleware(req, res, next) {
-    const token = req.headers.authorization.split(' ')[1];
-    if (!token) {
-        return res.status(403).json({ message: "No token provided" });
-    }
-    jwt.verify(token, 'shhh', (err, decoded) => {
-=======
 const key = process.env.JWT_SECRET_KEY;
 function authMiddleware(req, res, next) {
     const token = req.headers.authorization.split(" ")[1];
@@ -15,7 +6,7 @@ function authMiddleware(req, res, next) {
         return res.status(403).json({ message: "No token provided" });
     }
     jwt.verify(token, key, (err, decoded) => {
->>>>>>> b7efff1 (fresh code)
+
         if (err) {
             console.log(err)
             return res.status(401).json({ message: `Invalid token ${err}` });
